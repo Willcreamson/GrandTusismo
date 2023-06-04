8bb0c516be36bae9197991ea8b5674fea3b8e79d
Martyna0 <95086387+Martyna0@users.noreply.github.com>
2023-06-04 15:26:20 +0200

    TimeDistance creation v1

--- Backend/src/main/java/com/mongodb/quickstart/TimeDistance.java
+++ Backend/src/main/java/com/mongodb/quickstart/TimeDistance.java
@@ -1,4 +1,23 @@
 package com.mongodb.quickstart;
 
 public class TimeDistance {
+
+
+    public static double calculateMaxDistance(long dateStart, long dateEnd) {
+        //Function that will calculate the maximum distance we can parcour during the time (datestart and dateend) in parameters with the speed of 100 km/h
+        
+        long timeInML = dateEnd - dateStart;
+        
+        //Conversion ml in h
+        double timeInH = timeInML / (1000.0 * 60 * 60);
+
+        //Max distance with the speed of 100km/h
+        double maxDistance = 100.0 * timeInH;
+
+        return maxDistance;
+
+    }
+
+
+
 }
